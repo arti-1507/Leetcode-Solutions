196ac1dd8c03ef186c522a8723334a009b453d89
arti-1507 <artitabe15@gmail.com>
2026-02-18 21:54:02 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (65.08%) - LeetSync

---
+++ 953-reverse-only-letters/reverse-only-letters.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String reverseOnlyLetters(String s) {
+        char ch[]=s.toCharArray();
+        int i = 0, j = ch.length - 1;
+
+        while (i < j) {
+            if (!Character.isLetter(ch[i])) {
+                i++;
+            } else if (!Character.isLetter(ch[j])) {
+                j--;
+            } else {
+                char temp = ch[i];
+                ch[i] = ch[j];
+                ch[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        String str=new String(ch);
+        return str;
+    }
+}
