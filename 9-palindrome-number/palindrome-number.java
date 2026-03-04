6db1bcda0e27034257aae722b016370b20e9cda0
arti-1507 <artitabe15@gmail.com>
2026-03-04 22:53:28 +0530

    Time: 5 ms (78.11%) | Memory: 45.6 MB (97.00%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,19 +1,14 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) {
-            return false;
-        } else {
-            int original=x;
-            int new_x = 0;
-            while(x!=0){
-            int y=x%10;
-            new_x=new_x*10+y;
-            x=x/10;
-            }
-            return original==new_x;
-            
-            
+        int dummy=x;
+        int rev=0;
+        int rem;
+        while(dummy>0){
+            rem=dummy%10;
+            rev=rev*10+rem;
+            dummy/=10;
         }
+        return x==rev;
         
     }
 }
